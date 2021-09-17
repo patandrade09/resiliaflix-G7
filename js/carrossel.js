5841fff231df3fd6f1cf585202bb87631a6e2ed4
jeanetchichury <jean.etchichury@hotmail.com>
2021-09-17 14:45:55 -0300

    Carrossel finalizado com loop

---
+++ js/carrossel.js
@@ -0,0 +1,15 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:6,
+    loop:true,
+    margin:2,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
