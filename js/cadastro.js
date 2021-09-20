543ba9e9747d851c84241f146075b16f8ad478f2
eloisecoffe <61201063+eloisecoffe@users.noreply.github.com>
2021-09-19 21:00:17 -0300

    ajustando cadastro e add apis

---
+++ js/cadastro.js
@@ -0,0 +1,75 @@
+//parte API CEP
+$(document).ready(function () {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#rua").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+    }
+    //**//Quando o campo cep perde o foco.
+    $("#cep").blur(function () {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if (validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#rua").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#rua").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+                        $("#complemento").val(dados.complemento);
+                    } else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        // alert("CEP não encontrado.");
+                    }
+                });
+            } else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
+
+function validarEmail() {
+    var email = document.querySelector('#email');
+    var error = document.querySelector('#error-email');
+
+    if (!email.checkValidity()) {
+        error.innerHTML = "Email invalido";
+    }
+
+}
+
+function redefinirMsg() {
+    var error = document.querySelector('#error-email');
+    if (error.innerHTML == "Email invalido") {
+        error.innerHTML = "";
+    }
+}
